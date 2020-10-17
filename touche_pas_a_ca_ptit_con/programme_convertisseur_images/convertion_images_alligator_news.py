42f47fe0101605aeca559ac69cb112be075979fb
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2020-10-17 23:58:38 +0200

    bug images on Apple (fix ?)

--- touche_pas_a_ca_ptit_con/programme_convertisseur_images/convertion_images_alligator_news.py
+++ touche_pas_a_ca_ptit_con/programme_convertisseur_images/convertion_images_alligator_news.py
@@ -1,30 +1,32 @@
 # ------------------------------------
-#     Copyright : Thomas LÉPINE
+#     Copyright : Thomas LÉPINE  (thomas.lep4@gmail.com)
 # ------------------------------------
 
 from PIL import Image as imagePillow # Librairie Pillow (traitement d'images)
-import numpy as np # Numpy -> Traitement matricielle (NB : une image est une matrice composée pixels grosso modo)
-from resizeimage import resizeimage # Diminuer la taille d'une image pour moins de stockage
 import os # Librairie pour les fonctions interagissant avec le système (fichiers, ...)
 
 def fonctionsImage(image):
+	imageName = output_directory + filename.split('.')[0] + ".webp" # Nom de l'image finale
 	newImage = image.convert("RGB", palette=imagePillow.WEB) # Image.convert(mode=None, matrix=None, dither=None, palette=0, colors=256) # https://pillow.readthedocs.io/en/stable/_modules/PIL/Image.html#Image.convert
-	# print("Infos : " + image.mode + " -> mode / format -> " + image.format)
-	# print("Infos : " + newImage.mode + " -> mode")
-	if newImage.size[1] > 800: #Si la hauteur dépasse 800 pixels on redimensionne l'image
-		newImage = resizeimage.resize_height(newImage, 800)
+	newHeight = 800	
 
-	newImage.save(output_directory + filename.split('.')[0] + ".webp", "webp") #On enregistre l'image au bon format
+	if newImage.size[1] > newHeight: #Si la hauteur dépasse 800 pixels on redimensionne l'image
+		newWidth = newHeight * newImage.size[0] / newImage.size[1] # Calcul de la largeur pour garder les proportions de l'image
+		size = (newWidth, newHeight)
+		print("     -- New size (Width, Height) -> " + str(size))
+		# newImage = resizeimage.resize_height(newImage, 800, imagePillow.ANTIALIAS)
+		newImage.thumbnail(size, imagePillow.BICUBIC)
+
+	newImage.save(imageName, format="jpeg") #On enregistre l'image au bon format
 	newImage.close() # libère les ressources systèmes
 	image.close() # libère les ressources systèmes
-
 
 #General informations
 chemin_courant = os.getcwd()
 #print(chemin_courant)
 path_used = chemin_courant.split('\\')
 #print(path_used)
-#Recompose le chamin du fichier finalle
+#Recompose le chamin du fichier finale
 output_directory = ""
 for i in range(0, len(path_used)):
 	output_directory += path_used[i] + '\\'	
@@ -55,9 +57,9 @@ print("CHEMIN DE SORTIE : " + output_directory)
 	
 for filename in os.listdir(input_directory):	
 	print('> Nom de l\'image = ' + filename)
-	image = imagePillow.open(input_directory + filename)
+	image = imagePillow.open(input_directory + filename, 'r')
 	fonctionsImage(image)
-	nb_converti += 1	
+	nb_converti += 1
 
 print('\n')
 print("Fin du programme, " + str(nb_converti) + " image(s) ont été converti, appuyez sur une touche pour arrêter ...")
