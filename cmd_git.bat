22b95940fc573d4b8922533e509e1cddfebde552
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2020-11-29 16:23:42 +0100

    +

--- cmd_git.bat
+++ cmd_git.bat
@@ -1,9 +1,12 @@
 @echo off
 
+:: Commandes git ↓
 git add .
 git status
 set /p commitMessage = Message du commit :  
 git commit -m + %commitMessage%
 git push
+:: #################
 
+echo  
 set /p enter = Appuyez sur une touche pour terminer ...
