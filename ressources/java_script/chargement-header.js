2b4f14e8dbf0fe0caa6a4787edbe21b0298da3c0
Thomas-LEPINE <64126235+Thomas-LEPINE@users.noreply.github.com>
2021-04-16 21:01:19 +0200

    Finalisation des tests

--- ressources/java_script/chargement-header.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $("#header").load("../elements-html-to-load/header.html");
-});
--- ressources/java_script/chargement-navbar.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){
-    $("#navbar").load("../elements-html-to-load/navbar.html");
-});
